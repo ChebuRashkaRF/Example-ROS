cb9758725074a13c87c6f5e04092be89178cd03d
ChebuRashkaRF <50379603+ChebuRashkaRF@users.noreply.github.com>
2021-06-10 22:10:08 +0300

    Release v.1

---
+++ ros_example/example/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cheba/ros_example/example/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cheba/ros_example/example/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/cheba/ros_example/example:/opt/ros/noetic/share'
---
+++ ros_example/example/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/cheba/ros_example/example/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
